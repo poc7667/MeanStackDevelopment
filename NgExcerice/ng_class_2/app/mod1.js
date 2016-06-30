5af35f0dfe42bad12c03f5bc26190df92ac75d47
poc <poc7667@gmail.com>
2016-06-29 21:10:58 -0700

    add class 2 excercise in classroom

---
+++ NgExcerice/ng_class_2/app/mod1.js
@@ -0,0 +1,172 @@
+var app = angular.module('mod1', ['mod2']).controller("mod1Ctrl", function($scope) {
+
+    $scope.flights = [
+        {
+            "ident": "UAL1608",
+            "timestamp": "1434510446",
+            "longitude": "-105.07861",
+            "latitude": "38.31167",
+            "groundspeed": "420",
+            "altitude": "360",
+            "updateType": "TZ",
+            "heading": "274",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "UAL1284",
+            "timestamp": "1434510744",
+            "longitude": "-116.43275",
+            "latitude": "38.12796",
+            "groundspeed": "418",
+            "altitude": "360",
+            "updateType": "TA",
+            "heading": "265",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "UAL394",
+            "timestamp": "1434510464",
+            "longitude": "-120.12055",
+            "latitude": "37.02945",
+            "groundspeed": "526",
+            "altitude": "280",
+            "updateType": "TZ",
+            "heading": "83",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "UAL1681",
+            "timestamp": "1434510748",
+            "longitude": "-113.90917",
+            "latitude": "38.21631",
+            "groundspeed": "417",
+            "altitude": "360",
+            "updateType": "TA",
+            "heading": "275",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "VRD77",
+            "timestamp": "1434510767",
+            "longitude": "-107.96670",
+            "latitude": "38.37450",
+            "groundspeed": "439",
+            "altitude": "360",
+            "updateType": "TA",
+            "heading": "271",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "SKW6412",
+            "timestamp": "1434510897",
+            "longitude": "-122.56130",
+            "latitude": "37.79381",
+            "groundspeed": "340",
+            "altitude": "124",
+            "updateType": "TA",
+            "heading": "140",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "VRD222",
+            "timestamp": "1434510780",
+            "longitude": "-99.02999",
+            "latitude": "30.84779",
+            "groundspeed": "424",
+            "altitude": "255",
+            "updateType": "TA",
+            "heading": "103",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "UAL8",
+            "timestamp": "1434510780",
+            "longitude": "107.97712",
+            "latitude": "35.14437",
+            "groundspeed": "",
+            "altitude": "0",
+            "updateType": "TP",
+            "heading": "38",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "SWA4840",
+            "timestamp": "1434510440",
+            "longitude": "-121.91722",
+            "latitude": "36.81361",
+            "groundspeed": "345",
+            "altitude": "158",
+            "updateType": "TZ",
+            "heading": "341",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "UAL9",
+            "timestamp": "1434510780",
+            "longitude": "164.93977",
+            "latitude": "54.45394",
+            "groundspeed": "510",
+            "altitude": "400",
+            "updateType": "TP",
+            "heading": "236",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "CPA879",
+            "timestamp": "1434510445",
+            "longitude": "176.50000",
+            "latitude": "53.08333",
+            "groundspeed": "560",
+            "altitude": "340",
+            "updateType": "TZ",
+            "heading": "270",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "SKW6414",
+            "timestamp": "1434510477",
+            "longitude": "-122.12000",
+            "latitude": "38.14111",
+            "groundspeed": "319",
+            "altitude": "154",
+            "updateType": "TZ",
+            "heading": "19",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "UAL477",
+            "timestamp": "1434510477",
+            "longitude": "-117.86361",
+            "latitude": "40.24639",
+            "groundspeed": "481",
+            "altitude": "350",
+            "updateType": "TZ",
+            "heading": "68",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "DAL2027",
+            "timestamp": "1434510780",
+            "longitude": "-94.13847",
+            "latitude": "45.03816",
+            "groundspeed": "",
+            "altitude": "0",
+            "updateType": "TP",
+            "heading": "280",
+            "arrivalTime": "0"
+        },
+        {
+            "ident": "UAL1531",
+            "timestamp": "1434510780",
+            "longitude": "-82.94919",
+            "latitude": "34.22713",
+            "groundspeed": "464",
+            "altitude": "370",
+            "updateType": "TA",
+            "heading": "102",
+            "arrivalTime": "0"
+        }
+    ]
+
+
+})
